7c91b8f317889d2ad5f02252f96d0084eb77cf44
Stivet Fulgencio <stivet2184@gmail.com>
2025-06-19 10:45:37 -0500

    Enable CORS in spring boot app

---
+++ src/main/java/org/alpha/omega/hogwarts_artifacts_online/configuration/security/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package org.alpha.omega.hogwarts_artifacts_online.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**"); // Enable CORS for the entire app (all endpoints), for all incoming request
+            }
+        };
+    }
+}
